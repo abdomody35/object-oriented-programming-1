a1a98ff374f8e7661b35ce6afee45a9d35759b13
MahmoudFoaud <mfoad9259@gmail.com>
2024-05-15 13:04:39 +0300

    implemented tests for clear function

---
+++ Strings/test/clearTest.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include "..\Strings.cpp"
+using namespace std;
+int main() {
+    /*
+    test case 1 : the string is Mahmoud
+    input : no input (function should delete the string)
+    expected output : Empty string (check display tests to ensure correctness)
+    */
+    String str1("Mahmoud");
+    str1.clear();
+    str1.display();
+}
